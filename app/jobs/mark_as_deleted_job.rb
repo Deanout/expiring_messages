65b7c16c4f5662f9831b91e54aadec4f291e9167
Dean DeHart <DeHartDean@gmail.com>
2023-07-19 01:46:17 -0400

    Init commit

---
+++ app/jobs/mark_as_deleted_job.rb
@@ -0,0 +1,13 @@
+class MarkAsDeletedJob < ApplicationJob
+  # used for dom_id
+  include ActionView::RecordIdentifier
+  queue_as :default
+
+  def perform(post_id)
+    post = Post.find(post_id)
+    post.update(deleted: true)
+
+    # Turbo broadcast
+    Turbo::StreamsChannel.broadcast_remove_to("posts", target: dom_id(post))
+  end
+end
